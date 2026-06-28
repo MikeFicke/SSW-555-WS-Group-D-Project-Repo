cffd540b00cd068ac4f92647d9a381e5123188ad
Samantha Bryan <samanthabryan878@gmail.com>
2026-06-27 21:46:25 -0400

    Directory redesigning as we add more stories

---
+++ User Stories/US08_birth_after_marriage.py
@@ -0,0 +1,6 @@
+# Sam Bryan - Group D
+# Children should be born after marriage of parents (and not more than 9 months after their divorce)
+
+import datetime
+
+def validate_birth_after_marriage(individual_dict, families_dict):
