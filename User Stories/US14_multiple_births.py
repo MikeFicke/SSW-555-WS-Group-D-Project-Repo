b1b3c7eeaee3405af0b82ce2413ae46fe77b1003
Michael Ficke <mficke@stevens.edu>
2026-07-09 18:44:58 -0400

    feat: implement US14 to validate that no more than five siblings are born on the same date

---
+++ User Stories/US14_multiple_births.py
@@ -0,0 +1,31 @@
+# Group D - Michael Ficke
+# SSW-555-WS
+# User Story: No more than five siblings should be born at the same time.
+
+# Acknowledgement: IDE auto-complete was used for certain lines of code
+
+def validate_multiple_births(individual_dict, family_dict):
+    """
+    Check to make sure that no more than five siblings are born at the same time.
+    """
+    for family in family_dict:
+        children_ids = family_dict[family].get("Children")
+
+        if children_ids == [] or children_ids == None:
+            # skip, as there are no children to check for
+            continue
+        
+        children_birthdays = {}
+        for child in children_ids:
+            child_birthday = individual_dict[child].get("Birth")
+            if child_birthday == "NA" or child_birthday == None:
+                # skip, there is no birthday to look at
+                continue
+            if child_birthday in children_birthdays:
+                children_birthdays.append(child_birthday)
+            else:
+                children_birthdays.append(child_birthday)
+
+        for birthday, children in children_birthdays.items():
+            if len(children) > 5:
+                print(f"ERROR: US14: Family {family} has more than 5 children born on the same day ({birthday})")
